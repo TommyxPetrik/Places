852b55733fa9f1040406ee935527ef8ae5965897
Tommy <petrik@stud.uniza.sk>
2025-04-15 13:33:17 +0200

    authmiddleware const token

--- src/middlewares/authmiddleware.js
+++ src/middlewares/authmiddleware.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = async (req, res, next) => {
-  return next();
+  // return next();
   if (req.url.startsWith("/public")) {
     return next();
   }
-  const token = req.headers["x-access-token"];
+  // const token = req.headers["x-access-token"];
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2N2U2YmUyMjU4YTBmMDczNzlmYTgwYjMiLCJ1c2VybmFtZSI6IkNpZ2FuIiwidXNlcnJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ0NzE0MjE0fQ.7lI6C3OQig9Rk6Fhkqx1Lc4CC91Q2rPQEYTg_LjiDwE";
   if (!token) {
     return res.status(401).json({ message: "Chyba overenia totožnosti" });
   }
